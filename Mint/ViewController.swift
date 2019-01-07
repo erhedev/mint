6a028c2c98a4d71578a85b3551060b3e621c7089
krabbkakan <erikhede@gmail.com>
2019-01-07 09:45:45 +0100

    Initial Commit

---
+++ Mint/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mint
+//
+//  Created by Erik Hede on 07/01/2019.
+//  Copyright © 2019 Erik Hede. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
