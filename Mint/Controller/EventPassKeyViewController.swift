a1e318d1e2b97f897996b814aa23f6985a2cadca
krabbkakan <erikhede@gmail.com>
2019-01-08 10:54:52 +0100

    initial Commit Server list struct added EventPassKeyViewControllerAdded

--- Mint/Controller/EventPassKeyViewController.swift
+++ Mint/Controller/EventPassKeyViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class EventPassKeyViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ Mint/Model/ServerAdress.swift
@@ -0,0 +1,14 @@
+//
+//  ServerAdress.swift
+//  Mint
+//
+//  Created by Erik Hede on 08/01/2019.
+//  Copyright © 2019 Erik Hede. All rights reserved.
+//
+
+import Foundation
+
+struct ServerAddress {
+    static let live: String = "https://api.itmmobile.com/"
+    static let backup: String = "https://cloud.mintapp.net:8443/"
+}
